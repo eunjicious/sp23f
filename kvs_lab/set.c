630bf90a1e4c3d50f4159b0e0db89c0fb7575423
Eunji Lee <eunjicious@gmail.com>
2023-11-09 14:05:41 +0900

    minor revision

--- kvs_lab/set.c
+++ kvs_lab/set.c
@@ -1,8 +1,8 @@
 #include "kvs.h"
 
-int put(kvs_t* kvs, const char* key, const char* value)
+int set(kvs_t* kvs, const char* key, const char* value)
 {
-	printf("put: %s, %s\n", key, value);
+	printf("set: %s, %s\n", key, value);
 
 	/* do program here */
 	return 0;
